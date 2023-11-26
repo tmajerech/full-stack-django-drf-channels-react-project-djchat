07dade99694d79fc4e2f389120dd7f188a1b66a1
tmajerech <majerech.t@gmail.com>
2023-11-26 21:26:30 +0100

    added static file root

--- djchat/djchat/urls.py
+++ djchat/djchat/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -29,3 +31,6 @@ urlpatterns = [
                   path("api/docs/schema/", SpectacularAPIView.as_view(), name="schema"),
                   path("api/docs/schema/ui", SpectacularSwaggerView.as_view())
               ] + router.urls
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
